b2f3f4f672eb5d22c6d2c6f85fe126102c09c515
Dave Knoester <dave.knoester@gmail.com>
2014-04-26 14:17:04 -0400

    expr: 014-ca-movie

--- expr/014-ca-movie/config/test.sh
+++ expr/014-ca-movie/config/test.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 trap "exit 255" SIGINT SIGTERM
-PROG=${HOME}/bin/self-assembly-ca-1d-fsm
 
-function testfunction {
-    ${PROG} -l $1 --analyze ca_dom_1000x
-    ${PROG} -l $1 --analyze ca_movie
+function analyze {
+    # 1: executable
+    # 2: checkpoint
+    $1 -l $2 --analyze ca_dom_1000x
+    $1 -l $2 --analyze ca_movie
 }
 
-if [[ $# != 1 ]]; then
-    echo "Usage: $0 <path>"
+if [[ $# != 2 ]]; then
+    echo "Usage: $0 <executable> <path>"
+    echo "    executable: binary to run on checkpoints"
     echo "    path: location at which to search for checkpoints."
     exit -1
 fi
 
-for i in `find $1 -name "checkpoint-*.xml.gz"`; do
-    testfunction $i
+for i in `find $2 -name "checkpoint-*.xml.gz"`; do
+    DIR=`dirname $i`
+    CHECKPOINT=`basename $i`
+    pushd ${DIR} >/dev/null
+    analyze $1 ${CHECKPOINT}
+    popd >/dev/null
 done
